2a6e3958feb14cd1a46648eec4ab4517bf5325e2
zinee-u <suzinee.u@gmail.com>
2026-01-18 00:46:31 +0000

    Fix .gitignore

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -42,11 +42,6 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("/home/suzinee_u/workspace/rpi-zephyr/ethw6300/build/zephyr/cmake_install.cmake")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("/home/suzinee_u/workspace/rpi-zephyr/ethw6300/build/drivers/ethernet/cmake_install.cmake")
-endif()
-
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
